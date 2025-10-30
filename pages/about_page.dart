ac473a4acd19d4020efb42213f68e23f9314b203
Natss <168500422+Natssmmm@users.noreply.github.com>
2025-10-30 21:46:25 +0800

    Update about_page.dart

--- pages/about_page.dart
+++ pages/about_page.dart
@@ -1,37 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/pages/home_page.dart';
+import 'package:my_app/pages/projects_page.dart';
+import 'package:my_app/pages/gallery_page.dart';
+import 'package:my_app/pages/contact_page.dart';
 
-class AboutPage extends StatelessWidget{
+class AboutPage extends StatefulWidget{
+  @override
+  _MainNavigationPageState createState() => _MainNavigationPageState();
+}
+
+class _MainNavigationPageState extends State {
+  int _selectedIndex = 2;
+  final PageController _pageController = PageController(initialPage: 2);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('About Me'),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
+      body: PageView(
+        controller: _pageController,
+        children: [
+          AboutPage(),
+          ProjectsPage(),
+          HomePage(),
+          GalleryPage(),
+          ContactPage(),
+        ],
+        onPageChanged: (index){
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/profile.jpg'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Hi, I'm Nathaniel",
-              style: TextStyle(fontSize: 22,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "A passionate developer who loves building apps with flutter and exploring nex technologies.",
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onTabTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        elevation: 8.0,
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'About me'),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Projects'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.photo_library), label: 'Gallery'),
+          BottomNavigationBarItem(icon: Icon(Icons.contact_mail), label: 'Contact'),
+        ],
       ),
     );
+  }
+  void _onTabTapped(int index) {
+    setState((){
+      _selectedIndex = index;
+    });
+    _pageController.animateToPage(
+      index,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 }

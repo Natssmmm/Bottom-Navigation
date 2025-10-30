8f2b3db3e6d99dadf4920f655fef3434ad52fb9d
Natss <168500422+Natssmmm@users.noreply.github.com>
2025-10-30 21:52:10 +0800

    Update home_page.dart

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -11,18 +11,18 @@ class HomePage extends StatelessWidget {
     body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.flutter_dash, size: 80, color: Colors.blue),
-            SizedBox(height: 20),
-            Text(
-                'Welcome to My Portfolio App!',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text('Explore my projects, gallery, and contact me.'),
-          ],
-        ),
+        children: [
+          Icon(Icons.flutter_dash, size: 80, color: Colors.blue),
+          SizedBox(height: 20),
+          Text(
+            'Welcome to My Portfolio App!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text('Explore my projects, gallery, and contact me.'),
+        ],
       ),
-    );
+    ),
+  );
   }
 }
